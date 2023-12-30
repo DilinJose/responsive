cb70b64868e84ab8d4fcbe77ed5765e2be7314f5
divyajosekonikkara <divyajosekonikkara@gmail.com>
2023-12-30 16:37:05 +0530

    rotate button on hover

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='close d-flex justify-content-end w-100 p-5'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 31 31" fill="none">
+    <div className='close d-flex justify-content-end w-100 p-5 '>
+        <svg className='svg' xmlns="http://www.w3.org/2000/svg" width="31" height="31" viewBox="0 0 31 31" fill="none">
 <path d="M1.79175 1.79175L29.2084 29.2084" stroke="white" stroke-width="2" stroke-linecap="round"/>
 <path d="M1.79175 29.2084L29.2084 1.79175" stroke="white" stroke-width="2" stroke-linecap="round"/>
 </svg>
